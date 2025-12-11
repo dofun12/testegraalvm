9aa10ec3788aed034026effe32af2507bcba3fb2
kevim <k.v.n.such@gmail.com>
2025-12-10 21:20:21 -0300

    Initial commit

---
+++ src/main/java/org/lemanoman/testegraalvm/TestegraalvmApplication.java
@@ -0,0 +1,13 @@
+package org.lemanoman.testegraalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestegraalvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestegraalvmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lemanoman/testegraalvm/TestegraalvmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lemanoman.testegraalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestegraalvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
